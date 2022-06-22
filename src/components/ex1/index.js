b6ba05b95cf9fb01c7da23e82a1ae52d1027f1f7
LTH4823 <77533862+LTH4823@users.noreply.github.com>
2022-06-22 10:37:55 +0900

    update

---
+++ src/components/ex1/index.js
@@ -0,0 +1,37 @@
+import React, {useRef} from 'react';
+
+const jquery = window.jQuery
+//public 에 jqu
+
+const Ex1 = () => {
+
+    const taget = useRef()
+    //ref안에는 current 있고 이건 Dom의 요소다
+
+    const clickHide = () =>{
+        const ele = jquery(taget.current)
+        console.log(ele)
+
+        ele.hide(5000)
+    }
+    const clickShow = () =>{
+        const ele = jquery(taget.current)
+        console.log(ele)
+
+        ele.show('slow')
+    }
+
+    return (
+        <div>
+            <h1 ref={taget} className={"sample"}>Ex1</h1>
+
+            <div>
+                <button onClick={()=> clickHide()}>HIDE</button>
+                <button onClick={()=> clickShow()}>SHOW</button>
+
+            </div>
+        </div>
+    );
+};
+
+export default Ex1;
